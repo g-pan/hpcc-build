96e693719903f15ddf12b92405a2f15ccb00451f
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-09 13:25:38 -0500

    Enable bionic provision

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,11 @@ BINDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOTDIR=$( dirname "${BINDIR}")
 
 # Detech Unix OS environment
-
+distro=$(${BINDIR}/getpackagerevisionarch.sh | cut -d'_' -f 1)
+echo "OS $distro"
 
 # Setup base build environment
+${ROOTDIR}/distros/${distro}.sh
 
 # Setup plugin build environment
 
