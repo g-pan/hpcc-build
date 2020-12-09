b0fd5ac1a78b69e84887a7101846eb2b7a41244f
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-09 12:24:26 -0500

    fix if/else syntax for bash

--- bin/main.sh
+++ bin/main.sh
@@ -1,10 +1,8 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $OS | grep -i windows > /dev/null
-if ($? -eq 0) # Windows
-{
+if [ $? -eq 0 ] # Windows
+then
    powershell ${BINDIR}/build.ps1
-}
 else  # Unix
-{
    ${BINDIR}/build.sh
-}
+fi
