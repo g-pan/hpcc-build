0f7c1c14f2da390bb769f71c39c9f9117095789e
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-10 00:26:25 -0500

    test input 11

--- bin/main.sh
+++ bin/main.sh
@@ -1,7 +1,7 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "In main.sh"
-echo "INPUT_GITHUB_BRANCH: $INPUT_GITHUB_BRANCH"
+echo "INPUT_GITHUB_USER: $INPUT_GITHUB_USER"
 echo "INPUT_BUILD_CATEGORY: $INPUT_BUILD_CATEGORY"
 echo $OS | grep -i windows > /dev/null
 if [ $? -eq 0 ] # Windows
