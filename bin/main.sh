86a5082f72c04fce9373542fc5fc464fdaa7093e
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-10 12:41:45 -0500

    build docs 1

--- bin/main.sh
+++ bin/main.sh
@@ -1,10 +1,5 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "In main.sh"
-echo "INPUT_GITHUB_USER: ${INPUT_GITHUB_USER}"
-echo "INPUT_BUILD_CATEGORY: ${INPUT_BUILD_CATEGORY}"
-echo "INPUT_BUILD_CATEGORY 2: ${inputs.build_category}"
-
 
 echo $OS | grep -i windows > /dev/null
 if [ $? -eq 0 ] # Windows
