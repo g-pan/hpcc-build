0cae3aafcd3aedf70637850e2cdf11ce6831f8a3
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-09 22:54:21 -0500

    Check inputs

--- bin/main.sh
+++ bin/main.sh
@@ -1,4 +1,8 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "In mainsh"
+echo "INPUT_GITHUB_BRANCH: $INPUT_GITHUB_BRANCH"
+echo "INPUT_BUILD_CATEGORY: $INPUT_BUILD_CATEGORY"
 echo $OS | grep -i windows > /dev/null
 if [ $? -eq 0 ] # Windows
 then
