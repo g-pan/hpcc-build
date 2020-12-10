529c7003eb312b9d383b85a1966b2d6a731a5060
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-12-09 23:05:37 -0500

    test input in action.yml

--- bin/main.sh
+++ bin/main.sh
@@ -1,6 +1,6 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "In mainsh"
+echo "In main.sh"
 echo "INPUT_GITHUB_BRANCH: $INPUT_GITHUB_BRANCH"
 echo "INPUT_BUILD_CATEGORY: $INPUT_BUILD_CATEGORY"
 echo $OS | grep -i windows > /dev/null
@@ -8,5 +8,6 @@ if [ $? -eq 0 ] # Windows
 then
    powershell ${BINDIR}/build.ps1
 else  # Unix
-   ${BINDIR}/build.sh
+   echo "test"
+   #${BINDIR}/build.sh
 fi
